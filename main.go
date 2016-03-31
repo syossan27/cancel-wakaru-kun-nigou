72847ceddf012435ac933d2b4c3df7371a5e9e1b
syossan27 <wisdom1027@gmail.com>
2016-03-31 14:35:11 +0900

    commit

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-  "github.com/ant0ine/go-json-rest/rest"
-  "github.com/PuerkitoBio/goquery"
+  "vendor/github.com/ant0ine/go-json-rest/rest"
+  "vendor/github.com/PuerkitoBio/goquery"
   //"github.com/k0kubun/pp"
   "log"
   "fmt"
